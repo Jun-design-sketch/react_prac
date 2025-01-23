88caf7346b93dba72401c90e3e0185b15e2c3d98
jeong <jeong.junhueck@gmail.com>
2025-01-23 20:17:31 +0900

    isolated component instance / caution for update state

--- 250120_04_reactDeepDive(TickTackToe)/src/App.jsx
+++ 250120_04_reactDeepDive(TickTackToe)/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
+          {/* isolated component instance: PlayerComponentを繰り返すが、reactは各Playerを別のインスタンスとして扱う。 */}
             <Player name="Player 1" symbol="X"/>
             <Player name="Player 2" symbol="O"/>
         </ol>
--- 250120_04_reactDeepDive(TickTackToe)/src/components/Player.jsx
+++ 250120_04_reactDeepDive(TickTackToe)/src/components/Player.jsx
@@ -3,24 +3,30 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
-  function setEditing() {
-    setIsEditing(!isEditing);
+  function handleEditClick() {
+    // NOT RECOMMENDED
+    // setIsEditing(!isEditing);
+    // というのも、reactは即時に変更を行うのではない。未来に行うためにスケジュールする
+    // States updates are not performed instantly but as same point in the future
+    // In most cases, those state updates of course still are excuted almost instantly
+    // JSの非同期処理もそうだったが
+
+    // PASS A FUNCTION
+    setIsEditing((editing) => !editing);
+    // 以前の状態に基づくstateをsetするなら関数を使うこと
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required value={name}/>;
   }
 
   return (
     <li>
       <span className="player"></span>
-      {isEditing ? (
-        <>
-          <input type="text"></input>
-        </>
-      ) : (
-        <>
-          <span className="player-name">{name}</span>
-        </>
-      )}
+        {playerName}
       <span className="player-symbol">{symbol}</span>
-      <button onClick={setEditing}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
