2c429987cf6a4341f4fca43bb7737372653acf83
jeong <jeong.junhueck@gmail.com>
2025-02-06 00:10:44 +0900

    JSのCSS: importCSS, inline, styledComponents

--- 250223_07_styling/01-starting-project/src/components/AuthInputs.jsx
+++ 250223_07_styling/01-starting-project/src/components/AuthInputs.jsx
@@ -1,12 +1,50 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+
+// template literal... blahblah``
+// styledComponentsを用いることで
+// CSS属性を有するComponentを宣言できるようになる。
+// camelCaseせんともよい
+// これもscopedされたものとして使える。
+const CustomComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+// styledComponentsは動的に使用できないのか？
+// できる
+// styledComponentsはchildren propを持たないので<>の間にものを囲むことが可能
+// また、持っているpropを元となる要素に伝達する。
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${(props) => (props.$invalid ? "#f73f3f" : "transparent")};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,36 +55,47 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <CustomComponent>
         <p>
-          <label>Email</label>
-          <input
+          {/* CSSを組み合わせたstyledComponents */}
+          {/* <Label className={`label ${emailNotValid ? "invalid" : ""}`}> */}
+          {/* 内蔵属性と競合しないよう、$マークを先頭につける */}
+          <Label $invalid={emailNotValid}>Email</Label>
+          <Input
+            $invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            // style={{
+            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            // }}
+            className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          {/* <Label className={`label ${emailNotValid ? "invalid" : ""}`}> */}
+          <Label $invalid={passwordNotValid}>Password</Label>
+          <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            $invalid={passwordNotValid}
+            className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </CustomComponent>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
--- 250223_07_styling/01-starting-project/src/components/Header.jsx
+++ 250223_07_styling/01-starting-project/src/components/Header.jsx
@@ -1,11 +1,28 @@
 import logo from '../assets/logo.png';
+import classes from '../Header.module.css';
+// VanillaCSS
+// cssFile
+// 分業しやすい
+// Component毎にscopeされていないため競合し得る。= ファイルを分けても１つのファイルのようになってしまう。。
+// どうscopeする？
 
+// inline
+// Scopeできる。が、jsxとcssが混在する。
+// 一々要素毎にスタイルをつけないといけなくなる
+
+// .module.cssをimportし、cssファイル内のものを指定して使用することで
+// モジュール単位のCSS適用が容易になる
+// ただ、小さいCSSファイルが散らばるかも知れない
+
+// StyledComponent
+// styleをcomponentから管理する
+// npm install styled-components
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
